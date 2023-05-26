c01cf0fede37ea3eea114923f3637db44d6ad584
優吾 中林 <220159@jc-21.jp>
2023-05-26 10:09:24 +0900

    a

--- Direct3D.cpp
+++ Direct3D.cpp
@@ -1,8 +1,5 @@
 #include "Direct3D.h"
 
-const int WINDOW_WIDTH = 800;  //ウィンドウの幅
-const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
-
 //変数
 namespace Direct3D
 {
@@ -23,8 +20,8 @@ void Direct3D::Initialize(int winW, int winH, HWND hWnd)
     ZeroMemory(&scDesc, sizeof(scDesc));
 
     //描画先のフォーマット
-    scDesc.BufferDesc.Width = WINDOW_WIDTH;		//画面幅
-    scDesc.BufferDesc.Height = WINDOW_HEIGHT;	//画面高さ
+    scDesc.BufferDesc.Width = winW;		//画面幅
+    scDesc.BufferDesc.Height = winH;	//画面高さ
     scDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	// 何色使えるか
 
     //FPS（1/60秒に1回）
@@ -38,6 +35,35 @@ void Direct3D::Initialize(int winW, int winH, HWND hWnd)
     scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	//バックバッファの使い道＝画面に描画するために
     scDesc.SampleDesc.Count = 1;		//MSAA（アンチエイリアス）の設定
     scDesc.SampleDesc.Quality = 0;		//　〃
+
+    ////////////////上記設定をもとにデバイス、コンテキスト、スワップチェインを作成////////////////////////
+    D3D_FEATURE_LEVEL level;
+    D3D11CreateDeviceAndSwapChain(
+        nullptr,				// どのビデオアダプタを使用するか？既定ならばnullptrで
+        D3D_DRIVER_TYPE_HARDWARE,		// ドライバのタイプを渡す。ふつうはHARDWARE
+        nullptr,				// 上記をD3D_DRIVER_TYPE_SOFTWAREに設定しないかぎりnullptr
+        0,					// 何らかのフラグを指定する。（デバッグ時はD3D11_CREATE_DEVICE_DEBUG？）
+        nullptr,				// デバイス、コンテキストのレベルを設定。nullptrにしとけばOK
+        0,					// 上の引数でレベルを何個指定したか
+        D3D11_SDK_VERSION,			// SDKのバージョン。必ずこの値
+        &scDesc,				// 上でいろいろ設定した構造体
+        &pSwapChain,				// 無事完成したSwapChainのアドレスが返ってくる
+        &pDevice,				// 無事完成したDeviceアドレスが返ってくる
+        &level,					// 無事完成したDevice、Contextのレベルが返ってくる
+        &pContext);				// 無事完成したContextのアドレスが返ってくる
+
+    ///////////////////////////レンダーターゲットビュー作成///////////////////////////////
+    //スワップチェーンからバックバッファを取得（バックバッファ ＝ レンダーターゲット）
+    ID3D11Texture2D* pBackBuffer;
+    pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    //レンダーターゲットビューを作成
+    pDevice->CreateRenderTargetView(pBackBuffer, NULL, &pRenderTargetView);
+
+    //一時的にバックバッファを取得しただけなので解放
+    pBackBuffer->Release();
+
+
 }
 
 //描画開始
