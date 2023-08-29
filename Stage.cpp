6fe926d41d4934bf4dff7f1886a641114dab72cb
優吾 中林 <220159@jc-21.jp>
2023-08-29 16:08:55 +0900

    5つのブロック全部ロードして表示できた。 よっしゃあぁあああぁああぁあ？

--- Stage.cpp
+++ Stage.cpp
@@ -1,17 +1,41 @@
+#include <string>
 #include "Stage.h"
 #include "Engine/Input.h"
 #include "Engine/Model.h"
 
 Stage::Stage(GameObject* parent)
-	: GameObject(parent, "Stage"), hModel_(-1)
+	: GameObject(parent, "Stage")
 {
+	for (int i = 0; i < 5; i++)
+	{
+		hModel_[i] = -1;
+	}
+	for (int x = 0; x < XSIZE; x++)
+	{
+		for (int z = 0; z < ZSIZE; z++)
+		{
+			table_[x][z] = MODEL_TYPE::MODEL_DEFAULT;
+		}
+	}
 }
 
 void Stage::Initialize()
 {
+	std::string modelName[MODEL_NUM] =
+	{ 
+	 "BoxDefault.fbx",
+	 "BoxBrick.fbx",
+	 "BoxGrass.fbx",
+	 "BoxSand.fbx",
+	 "BoxWater.fbx"
+	};
+	std::string fname_base = "Assets\\";
 	//モデルデータのロード
-	hModel_ = Model::Load("Assets\\BoxDefault.fbx");
-	assert(hModel_ >= 0);
+	for (int i = 0; i < MODEL_NUM; i++)
+	{
+		hModel_[i] = Model::Load(fname_base + modelName[i]);
+		assert(hModel_[i] >= 0);
+	}
 }
 
 void Stage::Update()
@@ -20,13 +44,13 @@ void Stage::Update()
 
 void Stage::Draw()
 {
-	for (int x = 0; x < 15; x++)
+	for (int x = 0; x < XSIZE; x++)
 	{
-		for (int z = 0; z < 15; z++)
+		for (int z = 0; z < ZSIZE; z++)
 		{
-			transform_.position_ = XMFLOAT3(x, 0, z);
-			Model::SetTransform(hModel_, transform_);
-			Model::Draw(hModel_);
+			transform_.position_ = XMFLOAT3((float)x, 0, (float)z);
+			Model::SetTransform(hModel_[x / 3], transform_);
+			Model::Draw(hModel_[x / 3]);
 		}
 	}
 	
--- Stage.h
+++ Stage.h
@@ -1,10 +1,26 @@
 #pragma once
 #include "Engine/GameObject.h"
 
+namespace
+{
+	const int MODEL_NUM = 5;
+	const int XSIZE = 15;
+	const int ZSIZE = 15;
+}
+
 class Stage
 	:public GameObject
 {
-	int hModel_;
+	int hModel_[MODEL_NUM];
+	int table_[XSIZE][ZSIZE];
+	enum MODEL_TYPE
+	{
+		MODEL_DEFAULT,
+		MODEL_BRICK,
+		MODEL_GRASS,
+		MODEL_SAND,
+		MODEL_WATER,
+	};
 public:
 	Stage(GameObject* parent);
 
