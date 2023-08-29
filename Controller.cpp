44151c87e2be2d94057f20a8ac6672f781274422
優吾 中林 <220159@jc-21.jp>
2023-08-29 15:26:32 +0900

    上下視点移動できた

--- Controller.cpp
+++ Controller.cpp
@@ -10,14 +10,13 @@ Controller::Controller(GameObject* parent)
 void Controller::Initialize()
 {
 	transform_.position_ = XMFLOAT3(7, 1, 7);
-	Camera::SetPosition(XMFLOAT3(7, 16, 1));
-	
+	Camera::SetPosition(XMFLOAT3(7, 1, 7));
 }
 
 void Controller::Update()
 {
 	velocity_ = XMVectorSet(0, 0, 0, 0);
-	XMVECTOR cameraBase = XMVectorSet(0, 15, -5, 0);
+	XMVECTOR cameraBase = XMVectorSet(0, 0, -10, 0);
 
 	//前後左右移動
 	if (Input::IsKey(DIK_W))
@@ -37,15 +36,25 @@ void Controller::Update()
 		velocity_ += XMVectorSet(0.1f, 0.0f, 0.0f, 0.0f);
 	}
 	//どの方向も同じスピードにする
-	velocity_ = XMVector4Normalize(velocity_);
-	velocity_ /= 10;
+	velocity_ = XMVector4Normalize(velocity_); //正規化して全部1になる
+	velocity_ /= 10; //1じゃ速すぎるから割る
 
 	//回転
 	if (Input::IsKey(DIK_UP))
 	{
+		transform_.rotate_.x++;
+		if (transform_.rotate_.x >= 89.0f)
+		{
+			transform_.rotate_.x = 89;
+		}
 	}
 	if (Input::IsKey(DIK_DOWN))
 	{
+		transform_.rotate_.x--;
+		if (transform_.rotate_.x <= 0.0f)
+		{
+			transform_.rotate_.x = 0;
+		}
 	}
 	if (Input::IsKey(DIK_RIGHT))
 	{
@@ -56,8 +65,12 @@ void Controller::Update()
 		transform_.rotate_.y--;
 	}
 	//カメラ回転
+	XMMATRIX xrot = XMMatrixRotationX(XMConvertToRadians(transform_.rotate_.x));
 	XMMATRIX yrot = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));
-	XMVECTOR cameraRotVec = XMVector3Transform(cameraBase, yrot);
+
+	XMMATRIX rot = xrot * yrot;
+
+	XMVECTOR cameraRotVec = XMVector3Transform(cameraBase, rot);
 
 	XMVECTOR vPos = XMLoadFloat3(&transform_.position_);
 
--- Engine/Camera.cpp
+++ Engine/Camera.cpp
@@ -34,6 +34,16 @@ void Camera::SetPosition(XMFLOAT3 position)
 	SetPosition(XMLoadFloat3(&position));
 }
 
+XMVECTOR Camera::GetPosition()
+{
+	return position_;
+}
+
+XMVECTOR Camera::GetRotate()
+{
+	return XMVECTOR();
+}
+
 //è≈ì_Çê›íË
 void Camera::SetTarget(XMVECTOR target)
 {
