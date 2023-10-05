a89fa7f2753ff92ef2d5a189e09e1e00018a4dda
優吾 中林 <220159@jc-21.jp>
2023-10-05 12:30:02 +0900

    a

--- resource.h
+++ resource.h
@@ -3,6 +3,7 @@
 // MapEditor.rc ‚ÅŽg—p
 //
 #define IDD_DIALOG1                     101
+#define IDR_MENU1                       103
 #define IDC_RADIO_UP                    1018
 #define IDC_RADIO_DOWN                  1019
 #define IDC_RADIO6                      1020
@@ -10,14 +11,19 @@
 #define IDC_COMBO2                      1021
 #define IDC_BUTTON1                     1022
 #define IDC_RETURN                      1022
-#define IDC_LIST2                       1026
+#define ID_40001                        40001
+#define ID_40002                        40002
+#define ID_40003                        40003
+#define ID_MENU_NEW                     40004
+#define ID_MENU_OPEN                    40005
+#define ID_MENU_SAVE                    40006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40007
 #define _APS_NEXT_CONTROL_VALUE         1027
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
